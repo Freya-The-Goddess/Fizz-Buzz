2b35d1b402ca768a5d52040f8201cadeee18ae05
Freya-The-Goddess <106995979+Freya-The-Goddess@users.noreply.github.com>
2022-11-20 13:57:11 +1100

    FizzBuzz in Kotlin

---
+++ Kotlin/FizzBuzz.kts
@@ -0,0 +1,19 @@
+//FizzBuzz coding interview question in Kotlin Script
+//by Freya-The-Goddess
+
+val fizz: Int = 3
+val buzz: Int = 5
+val endNumber: Int = 100
+
+for (i in 1..endNumber) {
+    val output = if (i % fizz == 0 && i % buzz == 0) {
+        "FizzBuzz"
+    } else if (i % fizz == 0) {
+        "Fizz"
+    } else if (i % buzz == 0) {
+        "Buzz"
+    } else {
+        i.toString()
+    }
+    println(output)
+}
