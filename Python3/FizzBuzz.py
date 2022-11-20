5e776f41d0de6f8ecb96775d2a7b1d1f9bae9f42
Freya-The-Goddess <106995979+Freya-The-Goddess@users.noreply.github.com>
2022-11-20 13:52:11 +1100

    FizzBuzz in Python3

--- Python3/FizzBuzz.py
+++ Python3/FizzBuzz.py
@@ -1,12 +1,14 @@
 #FizzBuzz coding interview question in Python3
-#Freya-The-Goddess
+#by Freya-The-Goddess
 
 fizz = 3
 buzz = 5
 endNumber = 100
 
 for i in range(1, endNumber+1):
-    if i % fizz == 0:
+    if i % fizz == 0 and i % buzz == 0:
+        output = "FizzBuzz"
+    elif i % fizz == 0:
         output = "Fizz"
     elif i % buzz == 0:
         output = "Buzz"
