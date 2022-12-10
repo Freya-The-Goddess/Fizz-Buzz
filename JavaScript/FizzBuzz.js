13c889c4f039ce96819fc25083caaf169d2c98ea
Freya-The-Goddess <106995979+Freya-The-Goddess@users.noreply.github.com>
2022-12-10 23:12:21 +1100

    FizzBuzz in JavaScript

--- JavaScript/FizzBuzz.js
+++ JavaScript/FizzBuzz.js
@@ -1,24 +1,21 @@
 //FizzBuzz coding interview question in JavaScript
 //by Freya-The-Goddess
 
-function main() {
+function fizzbuzz() {
     var fizz = 3;
     var buzz = 5;
     var endNumber = 100;
     
-    var output = ""
+    var result = ""
     for (let i = 1; i <= endNumber; i++) {
-        if (i % fizz == 0 && i % buzz == 0) {
-            output += "FizzBuzz";
-        } else if (i % fizz == 0) {
-            output += "Fizz";
-        } else if (i % buzz == 0) {
-            output += "Buzz";
-        } else {
-            output += i.toString();
-        }
-        output += "\r\n"
+        output = ""
+        output += (i % fizz == 0 ? "Fizz" : "")
+        output += (i % buzz == 0 ? "Buzz" : "")
+        result += (output || i.toString()) + "\r\n"
     }
-    document.getElementById("result").textContent = output;
+    return result
 }
 
+function main() {
+    document.getElementById("result").textContent = fizzbuzz();
+}
